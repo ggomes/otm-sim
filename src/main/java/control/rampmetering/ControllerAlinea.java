b42aad62516104bd247ae0c644b5240adaeacf07
Gabriel Gomes <gomes@berkeley.edu>
2020-06-22 16:35:30 -0700

    queue override

--- src/main/java/control/rampmetering/ControllerAlinea.java
+++ src/main/java/control/rampmetering/ControllerAlinea.java
@@ -23,8 +23,12 @@ import static java.util.stream.Collectors.toSet;
 public class ControllerAlinea extends AbstractController {
 
     public Map<Long,AlineaParams> params;
-    public float cntrl_max_rate_vphpl;
-    public float cntrl_min_rate_vphpl;
+    public float cntrl_max_rate_vpspl;
+    public float cntrl_min_rate_vpspl;
+
+    // queue control
+    private boolean has_queue_control;
+    private float queue_threshold;
 
     ///////////////////////////////////////////////////
     // construction
@@ -33,14 +37,16 @@ public class ControllerAlinea extends AbstractController {
     public ControllerAlinea(Scenario scenario, Controller jaxb_controller) throws OTMException {
         super(scenario, jaxb_controller);
 
-        cntrl_max_rate_vphpl = Float.POSITIVE_INFINITY;
-        cntrl_min_rate_vphpl = Float.NEGATIVE_INFINITY;
+        cntrl_max_rate_vpspl = Float.POSITIVE_INFINITY;
+        cntrl_min_rate_vpspl = Float.NEGATIVE_INFINITY;
         if(jaxb_controller.getParameters()!=null){
             for(jaxb.Parameter p : jaxb_controller.getParameters().getParameter()){
                 if(p.getName().compareTo("max_rate_vphpl")==0)
-                    cntrl_max_rate_vphpl = Float.parseFloat(p.getValue());
+                    cntrl_max_rate_vpspl = Float.parseFloat(p.getValue())/3600f;
                 if(p.getName().compareTo("min_rate_vphpl")==0)
-                    cntrl_min_rate_vphpl = Float.parseFloat(p.getValue());
+                    cntrl_min_rate_vpspl = Float.parseFloat(p.getValue())/3600f;
+                if(p.getName().compareTo("queue_control")==0)
+                    has_queue_control = Boolean.parseBoolean(p.getValue());
             }
         }
     }
@@ -58,34 +64,16 @@ public class ControllerAlinea extends AbstractController {
             ActuatorMeter act = (ActuatorMeter) abs_act;
             AlineaParams param = new AlineaParams();
 
-//            AbstractLaneGroup lg = (AbstractLaneGroup) act.target;
-//            Link ml_link = lg.link;
-
             FixedSensor ml_sensor = (FixedSensor) sensors.iterator().next();
             Link ml_link = ml_sensor.get_link();
 
-//            AbstractLaneGroup target_lg = (AbstractLaneGroup) abs_act.target;
-//            Link onramp_link = target_lg.link;
-//
-//
-//            Node end_node = onramp_link.end_node;
-//            Set<Link> ml_links = end_node.out_links.stream()
-//                    .filter(link->link!=onramp_link).collect(toSet());
-//
-//            if(ml_links.size()!=1)
-//                throw new OTMException("ml_links.size()!=1");
-//
-//            Link ml_link = ml_links.iterator().next();
-
-
             Roadparam p = ml_link.road_param;
             param.gain_per_sec = p.getSpeed() * 1000f / 3600f / ml_link.length ; // [kph]*1000/3600/[m] -> [mps]
-//            param.gain_per_sec /= 10;    // reduce chatter
             float critical_density_vpkpl = p.getCapacity() / p.getSpeed();  // vpkpl
             param.target_density_veh = critical_density_vpkpl * ml_link.full_lanes * ml_link.length / 1000f;
 
-            param.max_rate_vps = Math.min(cntrl_max_rate_vphpl*act.total_lanes/3600f,act.max_rate_vps);
-            param.min_rate_vps = Math.max(cntrl_min_rate_vphpl*act.total_lanes/3600f,act.min_rate_vps);
+            param.max_rate_vps = Math.min(cntrl_max_rate_vpspl*act.total_lanes,act.max_rate_vps);
+            param.min_rate_vps = Math.max(cntrl_min_rate_vpspl*act.total_lanes,act.min_rate_vps);
             param.target_link = ml_link;
             params.put(abs_act.id , param);
 
@@ -97,26 +85,20 @@ public class ControllerAlinea extends AbstractController {
 
     @Override
     public void update_command(Dispatcher dispatcher) throws OTMException {
-
         for(AbstractActuator abs_act : this.actuators.values()){
             ActuatorMeter act = (ActuatorMeter) abs_act;
-            AlineaParams p = params.get(abs_act.id);
-            float density_veh = (float) p.target_link.get_veh();
-            float previous_rate_vps = ((CommandNumber) command.get(act.id)).value;
-            float rate_vps = previous_rate_vps +  p.gain_per_sec * (p.target_density_veh - density_veh);
-
-            if(rate_vps < 0f)
-                rate_vps = 0f;
-            if(rate_vps > p.max_rate_vps)
-                rate_vps = p.max_rate_vps;
-            if(rate_vps < p.min_rate_vps)
-                rate_vps = p.min_rate_vps;
-
-//            System.out.println(String.format("%.2f\t%.2f\t%.2f\t%f",scenario.dispatcher.current_time,p.target_density_veh,density_veh,rate_vps*3600f));
-
-            command.put(act.id,new CommandNumber(rate_vps));
-
+            AlineaParams p = params.get(act.id);
+            double queue = has_queue_control ? ((AbstractLaneGroup) act.target).link.get_veh() : 0f;
+            command.put(act.id,
+                    new CommandNumber( queue<queue_threshold ? compute_alinea_rate_vps(act,p) : p.max_rate_vps ) );
         }
+    }
+
+    private float compute_alinea_rate_vps(ActuatorMeter act,AlineaParams p){
+        float density_veh = (float) p.target_link.get_veh();
+        float previous_rate_vps = ((CommandNumber) command.get(act.id)).value;
+        float alinea_rate_vps = previous_rate_vps +  p.gain_per_sec * (p.target_density_veh - density_veh);
+        return Math.min( Math.max(alinea_rate_vps,p.min_rate_vps) , p.max_rate_vps );
     }
 
     public class AlineaParams {
--- src/main/java/control/rampmetering/ControllerFixedRate.java
+++ src/main/java/control/rampmetering/ControllerFixedRate.java
@@ -2,6 +2,8 @@ package control.rampmetering;
 
 import actuator.AbstractActuator;
 import actuator.AbstractActuatorLanegroupCapacity;
+import common.AbstractLaneGroup;
+import common.Link;
 import control.AbstractController;
 import control.command.CommandNumber;
 import dispatch.Dispatcher;
@@ -11,9 +13,12 @@ import common.Scenario;
 
 public class ControllerFixedRate extends AbstractController {
 
-    private float rate_vphpl;
-    private float max_rate_vphpl;
-    private boolean in_queue_override;
+    private float rate_vpspl;
+
+    // queue control
+    private boolean has_queue_control;
+    private float queue_threshold;
+    private float max_rate_vpspl;
 
     ///////////////////////////////////////////////////
     // construction
@@ -22,17 +27,20 @@ public class ControllerFixedRate extends AbstractController {
     public ControllerFixedRate(Scenario scenario, Controller jaxb_controller) throws OTMException {
         super(scenario, jaxb_controller);
 
-        max_rate_vphpl = Float.POSITIVE_INFINITY;
+        has_queue_control = false;
+        max_rate_vpspl = Float.POSITIVE_INFINITY;
         if(jaxb_controller.getParameters()!=null){
             for(jaxb.Parameter p : jaxb_controller.getParameters().getParameter()){
                 if(p.getName().compareTo("rate_vphpl")==0)
-                    rate_vphpl = Float.parseFloat(p.getValue());
+                    rate_vpspl = Float.parseFloat(p.getValue())/3600f;
                 if(p.getName().compareTo("max_rate_vphpl")==0)
-                    max_rate_vphpl = Float.parseFloat(p.getValue());
+                    max_rate_vpspl = Float.parseFloat(p.getValue())/3600f;
+                if(p.getName().compareTo("queue_control")==0)
+                    has_queue_control = Boolean.parseBoolean(p.getValue());
             }
         }
 
-
+        queue_threshold = Float.POSITIVE_INFINITY;
 
     }
 
@@ -40,8 +48,9 @@ public class ControllerFixedRate extends AbstractController {
     public void initialize(Scenario scenario) throws OTMException {
         super.initialize(scenario);
 
-        in_queue_override = true;
         update_command(scenario.dispatcher);
+        for(AbstractActuator act : actuators.values())
+            this.command.put(act.id, new CommandNumber(((AbstractActuatorLanegroupCapacity) act).total_lanes * rate_vpspl));
     }
 
     ///////////////////////////////////////////////////
@@ -51,33 +60,12 @@ public class ControllerFixedRate extends AbstractController {
     @Override
     public void update_command(Dispatcher dispatcher) throws OTMException {
 
-        if(in_queue_override){
-
-
-
-            // check queue
-            float queue = 0f;
-
-            // if queue > threshold keep max rate
-            float threshold = Float.POSITIVE_INFINITY;
-            float rate_vpspl;
-            if(queue>threshold){
-                rate_vpspl = Float.POSITIVE_INFINITY;
-            } else {
-                rate_vpspl = rate_vphpl / 3600f;
-                in_queue_override = false;
-            }
-
-            for(AbstractActuator act : actuators.values()) {
-                this.command.put(act.id, new CommandNumber(((AbstractActuatorLanegroupCapacity) act).total_lanes * rate_vpspl));
-            }
-
+        for(AbstractActuator act : actuators.values()) {
+            double queue = has_queue_control ? ((AbstractLaneGroup) act.target).link.get_veh() : 0f;
+            float qrate_vpspl = queue>queue_threshold ? max_rate_vpspl : rate_vpspl;
+            this.command.put(act.id, new CommandNumber(((AbstractActuatorLanegroupCapacity) act).total_lanes * qrate_vpspl));
         }
 
-    }
-
-    public void set_rate_vphpl(float x){
-        rate_vphpl = x;
     }
 
 }
